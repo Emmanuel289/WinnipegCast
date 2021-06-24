92c7c3096a04290e47e01ee8068a4378811806cc
Emmirald289 <emmirald@gmail.com>
2021-06-24 23:17:15 +0100

    Setup sequelize with database parameters

---
+++ backend/sequelize.js
@@ -0,0 +1,21 @@
+/**
+ * Import and instantiate Sequelize Object
+ */
+const Sequelize = require('sequelize');
+const config = require('./config');
+
+
+const c = config;
+
+// Instantiate new Sequelize instance
+const sequelize = new Sequelize({
+  "username": c.username,
+  "password": c.password,
+  "database": c.database,
+  "host":     c.host,
+
+  dialect: 'postgres',
+  storage: ':memory:'
+});
+
+module.exports = sequelize;
