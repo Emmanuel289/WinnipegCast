039b558859ea0e85ccf49644a5fe18e8dd792e84
Emmirald289 <emmirald@gmail.com>
2021-06-24 23:13:07 +0100

    Added a Sequelize client and connected to weather database

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,9 @@
+// Import all modules and dependencies for server and database
+require('dotenv').config();
 const express = require('express');
+const sequelize = require('./sequelize');
+
+
 
 const app = express();
 
@@ -10,9 +15,27 @@ app.use(json);
 const port = process.env.PORT || 8000;
 
 
+//Connect to postgres database
+
+try{
+
+    sequelize.authenticate();
+    
+
+}catch (error){
+
+    console.error('Unable to connect to the database:',error);
+}
+ 
+
+
+
+
+
+
 //Listen for connections
 app.listen(port, ()=>{
 
-    console.log(`Server is running on localhost://${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 })
 
