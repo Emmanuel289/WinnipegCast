05da0f6bca60b0349e7cf61a72b7542df99cc5af
Emmirald289 <emmirald@gmail.com>
2021-06-25 06:36:05 +0100

    Added endpoint for creating new weather records in database

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,22 @@
 require('dotenv').config();
 const express = require('express');
 const sequelize = require('./sequelize');
-
-
+const WeeklyForecast = require('./models/weekly_forecast');
 
 const app = express();
 
-const json = express.json
-
-app.use(json);
+const port = 8000;
 
 
-const port = process.env.PORT || 8000;
+app.use(express.json())
 
 
-//Connect to postgres database
+
+
+//Add the weekly forecast data model and connect to postgres database
+
+ WeeklyForecast.sync({ force: true })
+
 
 try{
 
@@ -27,15 +29,51 @@ try{
     console.error('Unable to connect to the database:',error);
 }
  
+//Create a new weekly forecast record
+
+app.post('/forecasts', 
+    async (req, res) => {
+    let currDate = req.body.date_time_local
+    let forecastTemps   = req.body.temperatures;
+
+    // check date is valid
+    if (!currDate) {
+        return res.status(400).send({ message: 'Date is required in format yyyy-mm-dd UCT format ' });
+    }
+
+    if(!forecastTemps){
+        
+        return res.status(400).send({message: 'Temperature forecast is required'});
+
+    }
+
+    const forecast = await new WeeklyForecast({
+            date_time_local: currDate,
+
+            temperatures: forecastTemps
+    });
+
+    const saved_forecast = await forecast.save();
+
+    res.status(201).send(saved_forecast);
+});
 
 
 
+// Get all weekly forecasts
+
+app.get('/forecasts/', async   (req, res) => {
+
+    const weekly_forecasts = await WeeklyForecast.findAll();
+
+    res.send(weekly_forecasts);
+
+    
+  });
 
 
 
 //Listen for connections
-app.listen(port, ()=>{
-
-    console.log(`Server is running on http://localhost:${port}`);
-})
-
+app.listen(port, () => {
+  console.log(` Server is listening on port ${port}!`)
+});
