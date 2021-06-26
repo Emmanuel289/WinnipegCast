d1d01cc7d227e17775ced3abf3fff1f16c4a6036
Emmirald289 <emmirald@gmail.com>
2021-06-26 10:53:04 +0100

    Added modified getter and poster callback functions

--- backend/routes/forecasts.routes.js
+++ backend/routes/forecasts.routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const CsvHandler = require("../controllers/csv-handler");
-const getRouter = require("../controllers/get-handler");
+const Getter = require("../controllers/get-handler");
+const createForecast  = require("../controllers/post-handler");
 const upload = require("../middleware/upload");
 
 let routes = (app) => {
@@ -9,13 +10,17 @@ let routes = (app) => {
     //Upload data set 
     router.post('/upload', upload.single("file"), CsvHandler.upload);
     
-    //Get all existing datasets
+    //Get all existing forecasts
     
-    router.get('/forecasts', CsvHandler.getForecasts);
+    router.get('/forecasts', Getter.getAll);
 
     //Get a day's forecast
 
-    router.get('/forecast/:date', getRouter );
+    router.get('/forecast/:date', Getter.getByDate);
+
+    //Add a new forecast
+
+    router.post('/forecasts', createForecast);
     
     //Use router middleware in server
     
